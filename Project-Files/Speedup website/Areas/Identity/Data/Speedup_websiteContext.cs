32ac2c77cea31f5379fd7552265bdc4338216404
Abbas-turkmani <77194041+Abbas-turkmani@users.noreply.github.com>
2023-01-14 18:17:33 +0200

    adding ui images

---
+++ Project-Files/Speedup website/Areas/Identity/Data/Speedup_websiteContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Speedup_website.Entitys.People;
+
+namespace Speedup_website.Areas.Identity.Data;
+
+public class Speedup_websiteContext : IdentityDbContext<User>
+{
+    public Speedup_websiteContext(DbContextOptions<Speedup_websiteContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
